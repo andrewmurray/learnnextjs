1f1e435978d717bfae83bea19b5bd467106f4a3b
Andrew Murray <sckoobs@gmail.com>
2018-01-25 17:20:50 +0000

    Updated to include server side URLs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
 
-const Index = () => (
-    <Layout>
-        <p>Hello Next.js</p>
-    </Layout>
+const PostLink = (props) => (
+    <li>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
 )
 
-export default Index
+export default () => (
+    <Layout>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink id="hello-nextjs" title="Hello next.js"/>
+            <PostLink id="learn-nextjs" title="Learn next.js is awesome"/>
+            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+        </ul>
+    </Layout>
+)
