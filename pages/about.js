c1df4e1c3ce43105efd3960de0cfb82e350c1e8f
Andrew Murray <sckoobs@gmail.com>
2018-02-01 16:51:09 +0000

    Starting to flesh out a calendar component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,24 @@
 import Layout from '../components/Layout'
 
-export default () => (
-    <Layout>
-        <p>This is the about page</p>
-    </Layout>
-)
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec']
+
+class Calendar extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            date: new Date()
+        }
+    }
+    render() {
+        let month = months[this.state.date.getMonth()]
+
+        return (
+            <Layout>
+                <div>{this.state.date.toString()}</div>
+                <div>{month}</div>
+            </Layout>
+        )
+    }
+}
+
+export default Calendar
